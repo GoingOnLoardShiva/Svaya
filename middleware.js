2d1b84001157266fd4407b3c3f9ba6c85d723f95
Your Name <your.email@example.com>
2025-12-09 19:11:10 +0530

    update midfdel ware

--- middleware.js
+++ middleware.js
@@ -1,29 +1,18 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 
 export function middleware(req) {
   const token = req.cookies.get("userToken")?.value;
 
-  // If no token → redirect to login
   if (!token) {
-    return NextResponse.redirect(new URL("/enrry/login", req.url));
-  }
-
-  try {
-    // Verify token
-    jwt.verify(token, process.env.JWT_SECRET);
-    return NextResponse.next(); // allow
-  } catch (error) {
-    // Invalid token → force logout
     return NextResponse.redirect(new URL("/entry/login", req.url));
   }
+
+  return NextResponse.next();
 }
 
-// Protect these routes
 export const config = {
   matcher: [
-    "/dashboard",  // Only logged in users can access dashboard
-    "/dashboard/:path*",  // Only logged in users can access dashboard
-    "/admin/:path*",      // You can add more protected routes
+    "/dashboard/:path*",
+    "/admin/:path*",
   ],
 };
